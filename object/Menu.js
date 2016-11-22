a1926d5665da004e8d442b72cf74ac179a78afe3
Tigri <imbert418@gmail.com>
2016-11-22 17:32:02 +0100

    Add the Menu file
    Signed-off-by: Tigri <imbert418@gmail.com>

---
+++ object/Menu.js
@@ -0,0 +1,19 @@
+function Menu() {}
+
+State
+
+/**
+ * Return if the new state of the game
+ **/
+
+Menu.prototype.update = function(user_input) {
+  //TODO
+}
+
+/**
+ * Draw the menu on the screen
+ **/
+
+Menu.prototype.draw = function() {
+  //TODO
+}
