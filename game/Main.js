1513958d84dd5e9b542e938fc0de1e4dae35c84c
Tigri <imbert418@gmail.com>
2016-11-24 14:51:32 +0100

    Get the user inputs
    Signed-off-by: Tigri <imbert418@gmail.com>

--- game/Main.js
+++ game/Main.js
@@ -1,11 +1,25 @@
-const Game = new require("./game.js")();
+const Game = require("./game.js");
 
-Game.initialise();
+const game = new Game.game();
+
+game.initialise();
+
+
+//User listener
+const body = document.getElementsByTagName("body")[0]
+
+body.addEventListener("keydown", function(e) {
+  game.add_user_input(Game.UserInput.KeyBoard(e.key))
+});
+
+body.addEventListener("keyup", function(e) {
+  game.del_user_input(Game.UserInput.KeyBoard(e.key))
+});
 
 function draw() {
   Initialiser();
-  Game.update();
-  Game.draw();
+  game.update();
+  game.draw();
 }
 
 Loop(-1);
