99778dd200417f21e84ff6b1f0422d76a69ec11d
Tigri <imbert418@gmail.com>
2016-11-30 16:04:56 +0100

    change draw to update
    Signed-off-by: Tigri <imbert418@gmail.com>

--- game/game.js
+++ game/game.js
@@ -208,7 +208,7 @@ Game.prototype.del_user_input = function(input) {
 Game.prototype.draw = function() {
   for (var ii = 0; ii < this.entities.length(); ii ++){
     this.entities.apply(ii, function(e) {
-      e.draw();
+      e.update();
     });
   }
 }
