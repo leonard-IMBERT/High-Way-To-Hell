62dab05146a4d8d06aa2947bcd304d3ea6ceaf25
BuildTools <unconfigured@null.spigotmc.org>
2016-11-30 16:12:00 +0100

    added weapons

--- object/Weapons.js
+++ object/Weapons.js
@@ -136,4 +136,7 @@ function TripleShot() {
 
 Weapons = {
   Basic: function(posX, posY) { return new Weapon(BasicShot, posX, posY, 20); }
+  Big: function(posX, posY) { return new Weapon(BigShot, posX, posY, 30);
+  Double: function(posX, posY) { return new Weapon(DoubleShot, posX, posY, 25); }
+  Triple: function(posX, posY) { return new Weapon(TripleShot, posX, posY, 25); }
 }
