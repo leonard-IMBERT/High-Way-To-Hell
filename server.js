0faeb961689a57a30295ff46dc6393d96be9bf50
Tigri <imbert418@gmail.com>
2017-02-28 20:59:15 +0100

    Add sprites folder to static endpoints

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.set('view engine','pug')
 
 app.use(express.static('css'))
 app.use(express.static('compile'))
+app.use(express.static('sprites'))
 
 app.get('/', (req, res) => {
   res.render('index')
